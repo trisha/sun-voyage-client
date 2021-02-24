619aaa93140b970dda7bd36dee716df5e4a97a49
yasaman forouzesh <yasamanforouzesh93@gmail.com>
2021-02-24 09:50:30 -0800

    pull to add styling

--- src/components/Planet/Planet.js
+++ src/components/Planet/Planet.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 import Comment from './Comment.js'
 import moment from 'moment'
 const axios = require('axios')
-const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
+// const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
+const REACT_APP_SERVER_URL ='http://localhost:8000'
 
 const Planet = (props) => {
 
--- src/components/User/Signup.js
+++ src/components/User/Signup.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
-const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+const REACT_APP_SERVER_URL ='http://localhost:8000'
+//const REACT_APP_SERVER_URL =process.env.REACT_APP_SERVER_URL;
 
 const Signup = () => {
     let [email, setEmail] = useState('')
