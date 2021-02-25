cecd764465d1b74751f5ed75560a42f5f8060aae
ElyssaW <ejwinch1@gmail.com>
2021-02-25 10:48:37 -0600

    stashing

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,12 +3,21 @@ import React, { useEffect, useState } from 'react';
 const Welcome = () => {
 
     return (
-      <div className='welcome-container-div' >
-        <div className='welcome-div' >
-              <h1 className='welcome-title title'>Welcome</h1>
-              <span className='welcome-subtitle'>Discover the Solar System</span>
+      <>
+      <div className='welcome-container-div anim' >
+        <div className='welcome-container-div anim flipped' >
+          <div className='welcome-container-div anim flipped-third' >
+          </div>
         </div>
       </div>
+
+      <div className='welcome-container-div'>
+        <div className='welcome-div' >
+          <h1 className='welcome-title title'>Welcome</h1>
+          <span className='welcome-subtitle'>Discover the Solar System</span>
+        </div>
+      </div>
+      </>
     );
 }
 
