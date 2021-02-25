9d93d97ab749165d10bff557f7cebae91b809f53
yasaman forouzesh <yasamanforouzesh93@gmail.com>
2021-02-25 08:19:39 -0800

    see coments and add

--- src/components/Planet/Planet.js
+++ src/components/Planet/Planet.js
@@ -13,30 +13,65 @@ const Planet = (props) => {
     const [planetData, setPlanetData] = useState({})
     // Yasamn added
     const [comments,setComments]=useState([])
+    const [newComment,setNewComment]=useState('')
 
     useEffect(() => {
         axios.get(`${REACT_APP_SERVER_URL}/planets/display/${props.planetId}`) 
         // Returns info on the planet.
         .then(rdata => {
             //-----> Elyssa I really don't know what information do you need but we should grad the information that we need from rdata and make our own objedt and push to planetdata
-            
+            console.log('🌹🌹🌹')
             console.log(rdata.data)
             
             //setPlanetData(PlanetData)
             setPlanetData({
+                id:rdata.data[0]._id,
                 name:rdata.data[0].name,
                 sideralOrbit:rdata.data[0].sideralOrbit,
                 mass:rdata.data[0].mass,
                 moons:rdata.data[0].moons
             })
-            setComments([...comments,rdata.data[0].comments])
+            let comments=rdata.data[0].comments.map(comment=>{
+                return{
+                    user:comment.user.name,
+                    content:comment.content
+                }
+            })
+            setComments([...comments,comments])
         }).catch(err=>{
         })
         
         
-    }, [props.refreshPage])
+    }, [])
 
+    // Yasaman added
+    const commentUpdate=(e)=>{
+        setNewComment(e.target.value)
+    }
+    const addCommentTodb=(e)=>{
+        e.preventDefault();
+        console.log("hello add comment")
+        axios({
+            url: `http://localhost:8000/comments/add/${planetData.id}`,
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`
+            },
+            data:{
+                'comment': newComment,
+                'userData': props.user 
+            }
+            
+            }).then(res=>{
+                // let comment
+                console.log('🎉🎉🎉🎉')
+                console.log(res)
+                setComments([])
+                setComments([...comments,res.data.searchTerm])
 
+            })
+    }
+    //.................................................................
     if (!planetData) {
         return (
             <p>Loading...</p>
@@ -44,9 +79,12 @@ const Planet = (props) => {
     } else {
             // Populate a value into comment list if any comments exist
             let commentList
-            if (planetData.comments) {
-                console.log(planetData)
-                commentList = planetData.comments.map((comment, i) => {
+            if (comments.length) {
+                console.log('❤❤')
+                commentList = comments.map((comment, i) => {
+                    // Yasaman quistion: It will takae long time to show new comments on page
+                    //----> Elyssa I don't know what information do you need from comments let me know so I can send more info from backend
+                    console.log(comment)
                     return < Comment comment={comment} user={props.user} key={`comment-id-${i}`} />
                 })
             } else {
@@ -117,8 +155,15 @@ const Planet = (props) => {
             <Row className='planet-comment-div'>
                     <h4 className='title bold comment-section-head'>Comments: </h4>
                     {commentList}
-        
-                    < Link to={`/comments/add/${planetData._id}`} ><button className='link-button'>Add To This Entry</button></Link>
+                    <form>
+                        <input type='text' onChange={(e)=>{commentUpdate(e)}}></input>
+
+
+                        <button className='link-button' onClick={(e)=>addCommentTodb(e)}>Add To This Entry</button>
+
+
+                    </form>
+                    {/* < Link to={`/comments/add/${planetData._id}`} ><button className='link-button'>Add To This Entry</button></Link> */}
                 </Row>
             </div>
             // <>
