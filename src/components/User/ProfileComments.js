92d020c21ee6d7243d5245c436b4e87564090ac3
yasaman forouzesh <yasamanforouzesh93@gmail.com>
2021-02-24 20:42:30 -0800

    original

--- src/components/User/ProfileComments.js
+++ src/components/User/ProfileComments.js
@@ -6,25 +6,6 @@ const axios = require('axios')
 
 const ProfileComments = (props) => {
     let [userComments, setUserComments] = useState([])
-    useEffect(() => {
-        axios({
-            url: `http://localhost:8000/auth/profile`,
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`
-            }
-            })
-            .then(res=>{
-                // console.log('✌✌✌')
-                // console.log(res)
-                return JSON.stringify(res.data.searchTerm) 
-                //setUserComments([...userComments,res.data.searchTerm])
-            })
-            .then(response=>{
-                setUserComments([...userComments,response])
-                console.log(response)
-            })
-    }, [])
 // props.user (currentUser and info)
 // props.planets (all planets and their infor)
     // const comments = props.planets.forEach( (planet,i) => {
